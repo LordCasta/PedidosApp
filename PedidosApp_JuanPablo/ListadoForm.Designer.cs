76f1b7e704215cc78907451403981d0f7beb1bb3
LordCasta <castanocastano6@gmail.com>
2025-04-30 00:38:53 -0500

    Feat: Se añade vista del form para listar

--- PedidosApp_JuanPablo/ListadoForm.Designer.cs
+++ PedidosApp_JuanPablo/ListadoForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace PedidosApp_JuanPablo
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PedidosApp_JuanPablo
 {
     partial class ListadoForm
     {
@@ -28,12 +31,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ListadoForm";
+            dtgPedidos = new DataGridView();
+            cmbFiltrarCategoria = new ComboBox();
+            label1 = new Label();
+            btnFiltrar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dtgPedidos).BeginInit();
+            SuspendLayout();
+            // 
+            // dtgPedidos
+            // 
+            dtgPedidos.AllowUserToDeleteRows = false;
+            dtgPedidos.AllowUserToOrderColumns = true;
+            dtgPedidos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgPedidos.Location = new Point(33, 23);
+            dtgPedidos.Name = "dtgPedidos";
+            dtgPedidos.ReadOnly = true;
+            dtgPedidos.Size = new Size(322, 338);
+            dtgPedidos.TabIndex = 0;
+            // 
+            // cmbFiltrarCategoria
+            // 
+            cmbFiltrarCategoria.FormattingEnabled = true;
+            cmbFiltrarCategoria.Location = new Point(565, 23);
+            cmbFiltrarCategoria.Name = "cmbFiltrarCategoria";
+            cmbFiltrarCategoria.Size = new Size(144, 23);
+            cmbFiltrarCategoria.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(486, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Categoría";
+            // 
+            // btnFiltrar
+            // 
+            btnFiltrar.Location = new Point(581, 75);
+            btnFiltrar.Name = "btnFiltrar";
+            btnFiltrar.Size = new Size(75, 23);
+            btnFiltrar.TabIndex = 3;
+            btnFiltrar.Text = "Filtrar";
+            btnFiltrar.UseVisualStyleBackColor = true;
+            btnFiltrar.Click += btnFiltrar_Click;
+            // 
+            // RegistroForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnFiltrar);
+            Controls.Add(label1);
+            Controls.Add(cmbFiltrarCategoria);
+            Controls.Add(dtgPedidos);
+            Name = "RegistroForm";
+            Text = "RegistroForm";
+            ((System.ComponentModel.ISupportInitialize)dtgPedidos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dtgPedidos;
+        private ComboBox cmbFiltrarCategoria;
+        private Label label1;
+        private Button btnFiltrar;
     }
 }
